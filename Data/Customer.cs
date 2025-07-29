e23d233f7c728b734df396d567685029068d83bc
Dao Chien <23a1001d0048@students.hou.edu.vn>
2025-07-29 19:16:53 +0700

    updateAdd project files.

---
+++ Data/Customer.cs
@@ -0,0 +1,9 @@
+public class Customer
+{
+    public int CustomerID { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public int AnnualIncome { get; set; }
+    public int SpendingScore { get; set; }
+    public string Segment { get; set; } // Nhóm phân c?m d? ?oán
+}
